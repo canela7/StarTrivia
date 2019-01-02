50c1618e24b24147f249a09d83ef442885fd63d3
Brian Canela <briancanela@brians-imac.local>
2019-01-02 12:26:48 -0800

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Brian Canela on 1/2/19.
+//  Copyright © 2019 Brian Canela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
